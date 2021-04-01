0beae1208ec9dee56dbdae5b1d95846fcad5ac73
Chase Webb <webb.chase.a@gmail.com>
2021-04-01 01:01:03 -0400

    Updated coding.
    Worked on scrape_mars code.

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,5 +1,23 @@
 #Import dependencies
-from flask import Flask
+from bs4 import BeautifulSoup
+import requests
+from splinter import Browser
+import pandas as pd
+import pymongo
 
-#Create Flask app
-app = Flask(__name__)
+#url to be scraped
+def init_browser():
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=False)
+    return browser
+
+def scrape():
+    browser = init_browser()
+
+    mars_scrape = {}
+
+    url = 'https://mars.nasa.gov/news/'
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    title_results = soup.find_all('div', class_='content_title')[0]
